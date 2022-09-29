c79e8d6c303e93634ca4f6c610bd034f04825c49
Supun Kanushka <skanushka@icloud.com>
2022-09-29 11:43:47 +0530

    Defined initial security groups

--- secondtest/prod.tf
+++ secondtest/prod.tf
@@ -14,3 +14,31 @@ resource "aws_s3_bucket_acl" "example_bucket_acl" {
 
 resource "aws_default_vpc" "default" {
 }
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "allow standard http/https inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0", "1.2.3.4/32"]
+  }
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1" # all protocols
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
